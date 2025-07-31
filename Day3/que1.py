53f21b8cdc5a1b50666ae53ac8e29a2ee5179c80
Kavyasreepattapu <22ht1a05b4@gmail.com>
2025-07-31 20:51:55 +0530

    third commit

---
+++ Day3/que1.py
@@ -0,0 +1,20 @@
+#1
+result = 10+3*2-8/4
+print(result)
+
+#2
+result = 4**2 +5/2*3
+print(result)
+
+#3
+result = (8+4)*3/2
+print(type(result),result)
+
+#4
+res = 16/4+2**3-6
+print(res)
+
+#5
+res = 10-3*(4+2)/5
+print(res)
+
